45c443646e06b307d4e3d9222baa9854c803e360
Shruti Nair <55981201+Shru890@users.noreply.github.com>
2022-07-26 19:33:15 +0530

    Create myjava.java

---
+++ myjava.java
@@ -0,0 +1,8 @@
+
+public class myjava {
+	public static void main(String[] args) {
+		System.out.println("This is Shruti here");
+		System.out.println("Thanks for connecting shruti .... feeling happy to work with you!!");
+	}
+
+}
